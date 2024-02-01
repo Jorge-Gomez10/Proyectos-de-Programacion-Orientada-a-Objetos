e1343f8fe0baddd2eaf35a4c8741a73aa6e81977
Jorge Gómez Tangoy <142865056+Jorge-Gomez10@users.noreply.github.com>
2024-01-31 23:27:46 -0500

    Programacion Orientada a Objetos

--- Dashboard.py
+++ Dashboard.py
@@ -19,8 +19,16 @@ def mostrar_menu():
     ruta_base = os.path.dirname(__file__)
 
     opciones = {
-        '1': 'Unidad 1/1.2. Tecnicas de Programacion/1.2-1. Ejemplo Tecnicas de Programacion.py',
-        # Agrega aquí el resto de las rutas de los scripts
+        '1': 'Unidad 1/1.2. Técnicas de Programación/ejemplo-1.py',
+        '2': 'Unidad 1/1.2. Técnicas de Programación/ejemplo-2.py',
+        '3': 'Unidad 1/2.1. Programacion tradicional frente a POO/PO_OBJETOS.py',
+        '4': 'Unidad 1/2.1. Programacion tradicional frente a POO/Programacion_tradicional.py',
+        '5': 'Unidad 1/2.2. Características de la POO/ejemplo_1.py',
+        '6': 'Unidad 1/2.2. Características de la POO/ejemplo_2.py',
+        '7': 'Unidad 2/1.1. Tipos de datos e identificadores/Tarea tipos de datos.py',
+        '8': 'Unidad 2/1.2. Clases, Objetos, Herencia, Encapsulamiento y Polimorfismo/Tarea-Clases-Objetos.py',
+        '9': 'Unidad 2/2.1. Constructores y Destructores/Tarea-Constructores-y-Destructores.py',
+
     }
 
     while True:
